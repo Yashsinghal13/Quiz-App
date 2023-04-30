bd01e8e16571aa1a315293d6b612867da9594448
Yash Singhal <yashsinghall123@gmail.com>
2023-04-30 11:53:13 +0530

    Quiz app

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,17 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+    public static String question[]={"1.Which company owns the android?","2.Which is not the Programming language",
+            "3.Which Language is used for the ios development?","4.Which of these was the first supercomputer?",
+            "5.What does the Internet prefix WWW stand for?","6.In what year was Google founded?","7.A network designed to allow communication within an organization is called:",
+            "8.Which of these is not a peripheral, in computer terms?","9.Which of these is not a computer language?","10.When was Microsoft founded?"
+
+
+    };
+    public static String choices[][]={{"Google","Apple","Microsoft","Samsung"},{"Java","Nodejs","Python","C++"}
+            ,{"Nodejs","Swift","Html","Css"},{"ENIAC","CDC 6600","Sierra","Cray-1"},{"Western Washington World","Wide Width Wickets","World Wide Web","Worldwide Weather"},
+            {"1998","1995","2001","1989"},{"an intranet","browser","the internet","the world wide web"},{"Keyboard","Mouse","Monitor","Motherboard"},{"COBOL","Hypertalk","Windows 2000","Ada"}
+            ,{"1980","1969","1986","1975"}
+    };
+    public static String correctAnswer[]={"Google","Nodejs","Swift","CDC 6600","World Wide Web","1998","an intranet","Motherboard","Windows 2000","1975"};
+}
